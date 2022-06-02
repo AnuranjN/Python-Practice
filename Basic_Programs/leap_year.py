e6abf21aaa9ceb3d4ee252a4c13c12dcf4bc1423
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-06-02 16:57:32 +0530

    Update leap_year.py

--- Basic_Programs/leap_year.py
+++ Basic_Programs/leap_year.py
@@ -1,8 +1,8 @@
 # program to check wether leap year
 
-year = int(input("Enter year"))
+year = int(input("Enter year"))                  #Take input from user
 
-if (year % 400 == 0) and (year % 100 == 0):
+if (year % 400 == 0) and (year % 100 == 0):       #Using conditional statement
     print("{0} is a leap year".format(year))
 
 elif (year % 4 ==0) and (year % 100 != 0):
